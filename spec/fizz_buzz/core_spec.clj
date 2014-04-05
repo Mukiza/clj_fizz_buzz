fcd3e7a7e96bc2ab3af9bf1c9310b26eb59fbc09
Andrew Mukiza <andrewmukiza@gmail.com>
2014-04-06 00:14:23 +0300

    reverting back input tp numbers and only converting to string when needed

--- spec/fizz_buzz/core_spec.clj
+++ spec/fizz_buzz/core_spec.clj
@@ -4,73 +4,73 @@
 
 (describe "FizzBuzz"
   (it "returns 1 when given 1"
-    (should= 1 (fizz-buzz "1")))
+    (should= 1 (fizz-buzz 1)))
 
   (it "returns 2 when given 2"
-      (should= 2 (fizz-buzz "2")))
+      (should= 2 (fizz-buzz 2)))
 
   (it "returns 'Fizz' when given 3"
-      (should= "Fizz" (fizz-buzz "3")))
+      (should= "Fizz" (fizz-buzz 3)))
 
   (it "returns 4 when given 4"
-      (should= 4 (fizz-buzz "4")))
+      (should= 4 (fizz-buzz 4)))
 
   (it "returns 'Buzz' when given 5"
-      (should= "Buzz" (fizz-buzz "5")))
+      (should= "Buzz" (fizz-buzz 5)))
           
   (it "returns 'Fizz' when given 6"
-      (should= "Fizz" (fizz-buzz "6")))
+      (should= "Fizz" (fizz-buzz 6)))
 
   (it "returns '7' when given 7"
-      (should= 7 (fizz-buzz "7")))
+      (should= 7 (fizz-buzz 7)))
 
   (it "returns '8' when given 8"
-      (should= 8 (fizz-buzz "8")))
+      (should= 8 (fizz-buzz 8)))
 
   (it "returns 'Fizz' when given 9"
-      (should= "Fizz" (fizz-buzz "9")))
+      (should= "Fizz" (fizz-buzz 9)))
 
   (it "returns 'Buzz' when given 10"
-      (should= "Buzz" (fizz-buzz "10")))
+      (should= "Buzz" (fizz-buzz 10)))
 
   (it "returns '11' when given 11"
-      (should= 11 (fizz-buzz "11")))
+      (should= 11 (fizz-buzz 11)))
 
   (it "returns 'Fizz' when given 12"
-      (should= "Fizz" (fizz-buzz "12")))
+      (should= "Fizz" (fizz-buzz 12)))
 
   (it "returns '13' when given 13"
-      (should= "Fizz" (fizz-buzz "13")))
+      (should= "Fizz" (fizz-buzz 13)))
 
   (it "returns '14' when given 14"
-      (should= 14 (fizz-buzz "14")))
+      (should= 14 (fizz-buzz 14)))
 
   (it "returns 'Fizz Buzz' when given 15"
-      (should= "Fizz Buzz" (fizz-buzz "15")))
+      (should= "Fizz Buzz" (fizz-buzz 15)))
 
   (it "returns '16' when given 16"
-      (should= 16 (fizz-buzz "16")))
+      (should= 16 (fizz-buzz 16)))
 
   (it "returns '17' when given 17"
-      (should= 17 (fizz-buzz "17")))
+      (should= 17 (fizz-buzz 17)))
 
   (it "returns '18' when given 18"
-      (should= "Fizz" (fizz-buzz "18")))
+      (should= "Fizz" (fizz-buzz 18)))
 
   (it "returns '19' when given 19"
-      (should= 19 (fizz-buzz "19")))
+      (should= 19 (fizz-buzz 19)))
 
   (it "returns '20' when given 20"
-      (should= "Buzz" (fizz-buzz "20")))
+      (should= "Buzz" (fizz-buzz 20)))
 
   (it "returns 'Fizz' when given 13"
-      (should= "Fizz" (fizz-buzz "13")))
+      (should= "Fizz" (fizz-buzz 13)))
 
   (it "returns 'Buzz' when given 52"
-      (should= "Buzz" (fizz-buzz "52")))
+      (should= "Buzz" (fizz-buzz 52)))
 
   (it "returns 'Buzz' when given 100"
-      (should= "Buzz" (fizz-buzz "100")))
+      (should= "Buzz" (fizz-buzz 100)))
 
   (it "returns 'Fizz Buzz' when given 150"
-      (should= "Fizz Buzz" (fizz-buzz "150"))))
+      (should= "Fizz Buzz" (fizz-buzz 150))))
--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,8 +1,8 @@
 (ns fizz_buzz.core)
 (defn fizz-buzz [number]
-  (cond (and (= 0 (rem (read-string number) 3)) (= 0 (rem (read-string number) 5))) "Fizz Buzz"
-        (or (= (read-string number) 3) (not= (.indexOf number "3") -1)) "Fizz"
-        (or (= (read-string number) 5) (not= (.indexOf number "5") -1)) "Buzz"
-        (= 0 (rem (read-string number) 3)) "Fizz"
-        (= 0 (rem (read-string number) 5)) "Buzz"
-        :else (read-string number)))
+  (cond (and (= 0 (rem number 3)) (= 0 (rem number 5))) "Fizz Buzz"
+        (or (=  number 3) (not= (.indexOf (Long/toString number) "3") -1)) "Fizz"
+        (or (=  number 5) (not= (.indexOf (Long/toString number) "5") -1)) "Buzz"
+        (= 0 (rem  number 3)) "Fizz"
+        (= 0 (rem  number 5)) "Buzz"
+        :else  number))

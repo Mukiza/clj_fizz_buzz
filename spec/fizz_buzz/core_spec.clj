ab26a795821f968adbfbf077c2d04a38c69e92d1
Andrew Mukiza <andrewmukiza@gmail.com>
2014-04-05 12:10:44 +0300

    added numbers upto 20 - stage 1 (Need to add stage two)

--- spec/fizz_buzz/core_spec.clj
+++ spec/fizz_buzz/core_spec.clj
@@ -19,4 +19,46 @@
       (should= "Buzz" (fizz-buzz 5)))
           
   (it "returns 'Fizz' when given 6"
-      (should= "Fizz" (fizz-buzz 6) )))
+      (should= "Fizz" (fizz-buzz 6)))
+
+  (it "returns '7' when given 7"
+      (should= 7 (fizz-buzz 7)))
+
+  (it "returns '8' when given 8"
+      (should= 8 (fizz-buzz 8)))
+
+  (it "returns 'Fizz' when given 9"
+      (should= "Fizz" (fizz-buzz 9)))
+
+  (it "returns 'Buzz' when given 10"
+      (should= "Buzz" (fizz-buzz 10)))
+
+  (it "returns '11' when given 11"
+      (should= 11 (fizz-buzz 11)))
+
+  (it "returns 'Fizz' when given 12"
+      (should= "Fizz" (fizz-buzz 12)))
+
+  (it "returns '13' when given 13"
+      (should= 13 (fizz-buzz 13)))
+
+  (it "returns '14' when given 14"
+      (should= 14 (fizz-buzz 14)))
+
+  (it "returns 'Fizz Buzz' when given 15"
+      (should= "Fizz Buzz" (fizz-buzz 15)))
+
+  (it "returns '16' when given 16"
+      (should= 16 (fizz-buzz 16)))
+
+  (it "returns '17' when given 17"
+      (should= 17 (fizz-buzz 17)))
+
+  (it "returns '18' when given 18"
+      (should= "Fizz" (fizz-buzz 18)))
+
+  (it "returns '19' when given 19"
+      (should= 19 (fizz-buzz 19)))
+
+  (it "returns '20' when given 20"
+      (should= "Buzz" (fizz-buzz 20))))
--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,6 +1,8 @@
 (ns fizz_buzz.core)
 (defn fizz-buzz [number]
-  (cond (= number 3) "Fizz"
+  (cond (and (= 0 (rem number 3)) (= 0 (rem number 5))) "Fizz Buzz"
+        (= number 3) "Fizz"
         (= number 5) "Buzz"
         (= 0 (rem number 3)) "Fizz"
+        (= 0 (rem number 5)) "Buzz"
         :else number))

3959914bcdaab983eedeb33000bc4551fc236758
Andrew Mukiza <andrewmukiza@gmail.com>
2014-04-05 16:06:25 +0300

    adding stage two (check contains, using strings instead of numbers)

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,8 +1,8 @@
 (ns fizz_buzz.core)
 (defn fizz-buzz [number]
-  (cond (and (= 0 (rem number 3)) (= 0 (rem number 5))) "Fizz Buzz"
-        (= number 3) "Fizz"
-        (= number 5) "Buzz"
-        (= 0 (rem number 3)) "Fizz"
-        (= 0 (rem number 5)) "Buzz"
-        :else number))
+  (cond (and (= 0 (rem (read-string number) 3)) (= 0 (rem (read-string number) 5))) "Fizz Buzz"
+        (or (= (read-string number) 3) (not= (.indexOf number "3") -1)) "Fizz"
+        (or (= (read-string number) 5) (not= (.indexOf number "5") -1)) "Buzz"
+        (= 0 (rem (read-string number) 3)) "Fizz"
+        (= 0 (rem (read-string number) 5)) "Buzz"
+        :else (read-string number)))

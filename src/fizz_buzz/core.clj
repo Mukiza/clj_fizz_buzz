33931542d96206f367b7ff10cab03f94588ee363
Andrew Mukiza <andrewmukiza@gmail.com>
2014-04-16 09:27:08 +0300

    refactoring the fizzbuzz fn

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,8 +1,18 @@
 (ns fizz_buzz.core)
+
+(defn divisible-by-three? [number]
+    (= 0 (rem number 3)))
+
+(defn divisible-by-five? [number]
+    (= 0 (rem number 5)))
+
+(defn contains? [number value] 
+    (not= (.indexOf (Long/toString number) value) -1))
+
 (defn fizz-buzz [number]
-  (cond (and (= 0 (rem number 3)) (= 0 (rem number 5))) "Fizz Buzz"
-        (or (=  number 3) (not= (.indexOf (Long/toString number) "3") -1)) "Fizz"
-        (or (=  number 5) (not= (.indexOf (Long/toString number) "5") -1)) "Buzz"
+  (cond (and (divisible-by-three? number) (divisible-by-five? number)) "Fizz Buzz"
+        (or (=  number 3) (contains? number "3")) "Fizz"
+        (or (=  number 5) (contains? number "5")) "Buzz"
         (= 0 (rem  number 3)) "Fizz"
         (= 0 (rem  number 5)) "Buzz"
         :else  number))
